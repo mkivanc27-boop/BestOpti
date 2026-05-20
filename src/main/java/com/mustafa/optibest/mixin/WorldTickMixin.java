ed38cd25974902caa78f7d4d164cbce57a12953e
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-20 23:44:54 +0300

    Update WorldTickMixin.java

--- src/main/java/com/mustafa/optibest/mixin/WorldTickMixin.java
+++ src/main/java/com/mustafa/optibest/mixin/WorldTickMixin.java
@@ -1,16 +1,16 @@
 package com.mustafa.optibest.mixin;
 
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.client.world.ClientWorld;
 
-@Mixin(World.class)
+@Mixin(ClientWorld.class) // Kendi hedef sınıfın neyse onunla değiştir
 public class WorldTickMixin {
+
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void skipWorldTicks(CallbackInfo ci) {
-        // Her 2 tick'te 1 kez çalıştır (Örnek: CPU kullanımı düşer)
+        // Tick atlama kodların burada olacak
     }
 }
-
