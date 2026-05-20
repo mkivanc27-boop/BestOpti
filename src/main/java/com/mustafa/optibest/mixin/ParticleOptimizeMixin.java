7f7ca8e2aadc08a2fa2273520622d916b1c84f17
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 00:36:37 +0300

    Update ParticleOptimizeMixin.java

--- src/main/java/com/mustafa/optibest/mixin/ParticleOptimizeMixin.java
+++ src/main/java/com/mustafa/optibest/mixin/ParticleOptimizeMixin.java
@@ -1,17 +1,17 @@
 package com.mustafa.optibest.mixin;
 
 import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.client.particle.Particle;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ParticleManager.class)
 public class ParticleOptimizeMixin {
-    // Parçacık limitini %50 azaltarak CPU üzerindeki yükü hafifletiyoruz
-    @ModifyArg(method = "addParticle", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"), index = 0)
-    private Object limitParticles(Object particle) {
-        // Burada basit bir kontrol ile çok fazla parçacık varsa eklemeyi reddedebilirsin
-        return particle;
+    // Metodu değiştirdik, @ModifyArg yerine @Inject ile daha güvenli bir noktaya (HEAD) ekliyoruz.
+    @Inject(method = "addParticle(Lnet/minecraft/client/particle/Particle;)V", at = @At("HEAD"), cancellable = true)
+    private void limitParticles(Particle particle, CallbackInfo ci) {
+        // Kodun çalışması için burayı şimdilik boş bırak, hata devam ederse burayı dolduracağız.
     }
 }
-
