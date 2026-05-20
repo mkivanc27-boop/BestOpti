545cf1eafe57047f133f80a77767b767cf562797
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-20 18:14:18 +0300

    Update EntityRenderMixin.java

--- src/main/java/com/mustafa/optibest/mixin/EntityRenderMixin.java
+++ src/main/java/com/mustafa/optibest/mixin/EntityRenderMixin.java
@@ -2,6 +2,7 @@ package com.mustafa.optibest.mixin;
 
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,11 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(EntityRenderer.class)
 public class EntityRenderMixin {
     @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
-    private void optimizeEntityRender(Entity entity, Object camera, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
-        // Çok uzaktaki varlıkların render'ını iptal ederek FPS'i korur
-        if (entity.distanceTo(entity.getOrigin()) > 64) {
+    private <T extends Entity> void optimizeEntityRender(T entity, Object camera, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
+        // 'getOrigin()' yerine 'getPos()' veya doğrudan koordinat karşılaştırması kullanıyoruz
+        // Minecraft 1.21'de entity'nin pozisyonunu almak için getPos() kullanılır
+        if (entity.getPos().squaredDistanceTo(x, y, z) > 64 * 64) {
             cir.setReturnValue(false);
         }
     }
 }
-
