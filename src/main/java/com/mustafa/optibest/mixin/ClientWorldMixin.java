e981ced0849b4f21dacec30e13186e089f2c3db7
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 00:16:55 +0300

    Create ClientWorldMixin.java

---
+++ src/main/java/com/mustafa/optibest/mixin/ClientWorldMixin.java
@@ -0,0 +1,7 @@
+package com.mustafa.optibest.mixin;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ClientWorld.class)
+public class ClientWorldMixin { }
+
