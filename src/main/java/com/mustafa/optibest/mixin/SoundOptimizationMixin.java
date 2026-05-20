90e53246828e8efd5dbdf26b7fce5d46620eaea0
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 00:17:49 +0300

    Create SoundOptimizationMixin.java

---
+++ src/main/java/com/mustafa/optibest/mixin/SoundOptimizationMixin.java
@@ -0,0 +1,7 @@
+package com.mustafa.optibest.mixin;
+import net.minecraft.client.sound.SoundManager;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SoundManager.class)
+public class SoundOptimizationMixin { }
+
