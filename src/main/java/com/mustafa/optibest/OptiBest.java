3cb9940a30ab4316c1107981f697c83ce5ed82c5
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-20 21:21:09 +0300

    Update OptiBest.java

--- src/main/java/com/mustafa/optibest/OptiBest.java
+++ src/main/java/com/mustafa/optibest/OptiBest.java
@@ -1,10 +1,13 @@
 package com.mustafa.optibest;
+
 import net.fabricmc.api.ModInitializer;
 
 public class OptiBest implements ModInitializer {
     @Override
     public void onInitialize() {
-        // OptiBest basariyla yuklendi!
+        // Ayarlar menüsünü (MidnightLib) başlatır
+        OptiBestConfig.init("optibest", OptiBestConfig.class);
+        
+        System.out.println("OptiBest Modu basariyla aktif edildi! FPS ucurmaya hazir.");
     }
 }
-
