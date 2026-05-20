794c927e5d6d404aa249a870a8a17611e51963bd
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-20 23:45:58 +0300

    Update OptiBestLightMixin.java

--- src/main/java/com/mustafa/optibest/mixin/OptiBestLightMixin.java
+++ src/main/java/com/mustafa/optibest/mixin/OptiBestLightMixin.java
@@ -1,16 +1,16 @@
 package com.mustafa.optibest.mixin;
 
-import net.minecraft.world.chunk.light.LightingProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.world.chunk.light.LightingProvider;
 
-@Mixin(LightingProvider.class)
+@Mixin(LightingProvider.class) // Kendi hedef sınıfın neyse onunla değiştir
 public class OptiBestLightMixin {
+
     @Inject(method = "doLightUpdates", at = @At("HEAD"), cancellable = true)
     private void skipLaggyLightUpdates(CallbackInfo ci) {
-        // Işık güncellemelerini seyrelt
+        // Işık güncelleme optimizasyon kodların burada olacak
     }
 }
-
