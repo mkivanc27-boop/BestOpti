b82d02960efb83e8a64aa93b03c351f20d35ef2f
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 00:15:48 +0300

    Create ChunkRenderMixin.java

---
+++ src/main/java/com/mustafa/optibest/mixin/ChunkRenderMixin.java
@@ -0,0 +1,10 @@
+package com.mustafa.optibest.mixin;
+
+import net.minecraft.client.render.chunk.ChunkRendererRegion;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ChunkRendererRegion.class)
+public class ChunkRenderMixin {
+    // Chunk render yükünü hafifletmek için şablon
+}
+
