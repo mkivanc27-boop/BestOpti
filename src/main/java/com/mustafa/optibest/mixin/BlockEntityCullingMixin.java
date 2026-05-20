f0cae6dd292238dd254b933d6d78a24f35a419c5
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 00:28:53 +0300

    Update BlockEntityCullingMixin.java

--- src/main/java/com/mustafa/optibest/mixin/BlockEntityCullingMixin.java
+++ src/main/java/com/mustafa/optibest/mixin/BlockEntityCullingMixin.java
@@ -1,9 +1,10 @@
 package com.mustafa.optibest.mixin;
 
-import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(BlockEntityRenderer.class)
+// Artık bir interface değil, somut bir sınıf hedefliyoruz
+@Mixin(BlockEntityRenderDispatcher.class)
 public class BlockEntityCullingMixin {
+    // Render işlemini burada culling (görünmeyenleri eleme) mantığıyla bağlayacağız.
 }
-
