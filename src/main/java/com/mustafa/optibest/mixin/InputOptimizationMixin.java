dc9164463fbc4396af663c88475d69279717df45
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 00:18:21 +0300

    Create InputOptimizationMixin.java

---
+++ src/main/java/com/mustafa/optibest/mixin/InputOptimizationMixin.java
@@ -0,0 +1,7 @@
+package com.mustafa.optibest.mixin;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftClient.class)
+public class InputOptimizationMixin { }
+
