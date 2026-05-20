754a8ae849d769e695a50703e6fdf46566b85af3
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 00:18:38 +0300

    Create ResourceReloadMixin.java

---
+++ src/main/java/com/mustafa/optibest/mixin/ResourceReloadMixin.java
@@ -0,0 +1,7 @@
+package com.mustafa.optibest.mixin;
+import net.minecraft.resource.ReloadableResourceManagerImpl;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ReloadableResourceManagerImpl.class)
+public class ResourceReloadMixin { }
+
