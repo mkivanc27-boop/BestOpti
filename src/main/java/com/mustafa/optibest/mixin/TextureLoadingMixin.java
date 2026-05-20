85c9f819764b3fb72d39417e3597bce661739f46
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 00:18:05 +0300

    Create TextureLoadingMixin.java

---
+++ src/main/java/com/mustafa/optibest/mixin/TextureLoadingMixin.java
@@ -0,0 +1,7 @@
+package com.mustafa.optibest.mixin;
+import net.minecraft.client.texture.TextureManager;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TextureManager.class)
+public class TextureLoadingMixin { }
+
