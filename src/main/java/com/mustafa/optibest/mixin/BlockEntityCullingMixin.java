f3dd0bbb476cea38ea9069a9c87be9475f0665d4
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 00:16:17 +0300

    Create BlockEntityCullingMixin.java

---
+++ src/main/java/com/mustafa/optibest/mixin/BlockEntityCullingMixin.java
@@ -0,0 +1,9 @@
+package com.mustafa.optibest.mixin;
+
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(BlockEntityRenderer.class)
+public class BlockEntityCullingMixin {
+}
+
