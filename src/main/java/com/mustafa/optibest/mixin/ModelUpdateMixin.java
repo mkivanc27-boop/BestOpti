2860eb0b8464bd7109cc183c2a38abc5da722853
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 00:17:32 +0300

    Create ModelUpdateMixin.java

---
+++ src/main/java/com/mustafa/optibest/mixin/ModelUpdateMixin.java
@@ -0,0 +1,7 @@
+package com.mustafa.optibest.mixin;
+import net.minecraft.client.render.entity.model.EntityModel;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(EntityModel.class)
+public class ModelUpdateMixin { }
+
