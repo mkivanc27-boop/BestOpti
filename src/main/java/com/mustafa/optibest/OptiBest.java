f4fadae6eb6a8611a3c86181c8bc5d00ab86f2ff
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-20 17:40:07 +0300

    Create OptiBest.java

---
+++ src/main/java/com/mustafa/optibest/OptiBest.java
@@ -0,0 +1,10 @@
+package com.mustafa.optibest;
+import net.fabricmc.api.ModInitializer;
+
+public class OptiBest implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // OptiBest basariyla yuklendi!
+    }
+}
+
