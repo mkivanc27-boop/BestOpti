862c887d7e9f93f773dbc1577162f59ec41b0a3d
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-20 23:46:26 +0300

    Update OptiBestCullingMixin.java

--- src/main/java/com/mustafa/optibest/mixin/OptiBestCullingMixin.java
+++ src/main/java/com/mustafa/optibest/mixin/OptiBestCullingMixin.java
@@ -1,16 +1,16 @@
 package com.mustafa.optibest.mixin;
 
-import net.minecraft.client.render.chunk.ChunkRendererRegion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.client.render.chunk.ChunkRendererRegion;
 
 @Mixin(ChunkRendererRegion.class)
 public class OptiBestCullingMixin {
 
     @Inject(method = "shouldShow", at = @At("HEAD"), cancellable = true)
     private void optimizeChunkRendering(CallbackInfoReturnable<Boolean> cir) {
-        // Şu an boş, sadece çalışıp çalışmadığını test ediyoruz
+        // Culling (gereksiz blokları gizleme) kodların burada olacak
     }
 }
