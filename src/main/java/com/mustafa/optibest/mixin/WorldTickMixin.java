390fdd08f8b91b62ac85e04159a5e44a683cc96e
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 15:12:59 +0300

    Update WorldTickMixin.java

--- src/main/java/com/mustafa/optibest/mixin/WorldTickMixin.java
+++ src/main/java/com/mustafa/optibest/mixin/WorldTickMixin.java
@@ -5,18 +5,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.function.BooleanSupplier;
 
 @Mixin(ClientWorld.class)
 public class WorldTickMixin {
 
-    private int ambientSoundTick = 0;
+    private int tickCounter = 0;
 
     @Inject(method = "tickEntities", at = @At("HEAD"), cancellable = true)
     private void throttleEntityTicking(CallbackInfo ci) {
-        ambientSoundTick++;
-        // Her 2 tick'te bir entity tick'i atla
-        // Bu, yüzlerce entity varken CPU'yu yarı yarıya rahatlatır
-        if (ambientSoundTick % 2 != 0) {
+        tickCounter++;
+        if (tickCounter % 2 != 0) {
             ci.cancel();
         }
     }
