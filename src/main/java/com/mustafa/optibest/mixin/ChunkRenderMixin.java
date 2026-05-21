096516031bcbde57cbb4fb00e22c362f73c62208
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-21 15:03:07 +0300

    Update ChunkRenderMixin.java

--- src/main/java/com/mustafa/optibest/mixin/ChunkRenderMixin.java
+++ src/main/java/com/mustafa/optibest/mixin/ChunkRenderMixin.java
@@ -1,42 +1,31 @@
 package com.mustafa.optibest.mixin;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.Frustum;
-import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.chunk.ChunkBuilder;
-import net.minecraft.util.math.Box;
+import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ChunkBuilder.BuiltChunk.class)
+@Mixin(GameRenderer.class)
 public class ChunkRenderMixin {
 
-    @Inject(
-        method = "isVisible",
-        at = @At("HEAD"),
-        cancellable = true
-    )
-    private void frustumCullChunk(Frustum frustum, CallbackInfoReturnable<Boolean> cir) {
-        ChunkBuilder.BuiltChunk self = (ChunkBuilder.BuiltChunk)(Object)this;
+    @Inject(method = "render", at = @At("HEAD"))
+    private void optimizeRenderCall(CallbackInfo ci) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client == null || client.world == null) return;
 
-        // Chunk'ın dünya koordinatlarındaki bounding box'ını al
-        Box boundingBox = new Box(
-            self.getOrigin().getX(),
-            self.getOrigin().getY(),
-            self.getOrigin().getZ(),
-            self.getOrigin().getX() + 16,
-            self.getOrigin().getY() + 16,
-            self.getOrigin().getZ() + 16
-        );
+        // Render distance'ı dinamik olarak ayarla
+        // Düşük FPS'te render mesafesini azalt
+        int fps = client.getCurrentFps();
+        int currentDistance = client.options.getViewDistance().getValue();
 
-        // Frustum dışındaysa render etme
-        if (!frustum.isVisible(boundingBox)) {
-            cir.setReturnValue(false); // Görünmüyor, atla
-            return;
+        if (fps < 20 && currentDistance > 6) {
+            client.options.getViewDistance().setValue(6);
+        } else if (fps < 30 && currentDistance > 8) {
+            client.options.getViewDistance().setValue(8);
         }
-
-        // Görünüyorsa normal devam et
-        cir.setReturnValue(true);
     }
 }
